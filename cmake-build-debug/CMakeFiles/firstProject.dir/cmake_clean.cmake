36af0dc70fe4e9e1bffda8a87c07b31f5ca71950
asmita15 <asmita.s.ghutukade@gmail.com>
2018-07-23 20:29:59 +0530

    this is cline based first commit

---
+++ cmake-build-debug/CMakeFiles/firstProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstProject.dir/main.cpp.obj"
+  "firstProject.pdb"
+  "firstProject.exe"
+  "firstProject.exe.manifest"
+  "libfirstProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
